df13fd1f96ba40cb852b9c0dae7091759ea9ecff
Huib van Nieuwenhuijze <Huib.van.Nieuwenhijze@gmail.com>
2021-12-01 12:49:38 +0100

    First commit

---
+++ base.py
@@ -0,0 +1,27 @@
+#!python3
+import os
+import datetime
+import re
+
+# Test input
+input = ["939","7,13,x,x,59,x,31,19"]
+
+# input integers separated by commas
+#input= [int(i) for i in open(os.path.basename(__file__).split(".")[0]+'.input').read().split(",")]
+
+# input complete lines
+input = open(os.path.basename(__file__).split(".")[0]+'.input').read().splitlines()
+
+def solve1():
+    print("Deel 1: No")
+
+def solve2():
+    print("Deel 2: No")
+
+start = datetime.datetime.now()
+solve1()
+print("Tijd voor oplossing 1: ", datetime.datetime.now()-start)
+
+start = datetime.datetime.now()
+solve2()
+print("Tijd voor oplossing 2: ", datetime.datetime.now()-start)
